304cb2984e9338faf07938da5286edc4d9ff803c
burning5 <burning05@gmail.com>
2014-09-17 18:21:38 +0900

    account popup 제작

--- js/event.js
+++ js/event.js
@@ -5,5 +5,16 @@ $(function(){
 		var winHeight = $(window).height();
 		$('.container').css({'min-height':winHeight - 67});
 	});
+
+	/* 팝업 탭 기능 */
+	(function(){
+		var popTabArea = $( '.popup_tab_area > section' );
+		var popTabBtns = $( '.popup_section_heading a' );
+		popTabBtns.on( 'click', function(e){
+			e.preventDefault();
+			popTabArea.removeClass('on');
+			$( $( this ).attr( 'href' ) ).addClass('on');
+		});
+	})();
 	
 });
