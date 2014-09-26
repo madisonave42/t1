3aa5ad404d9b208f8c769041440d01d5bf9fc00d
junsik <burning05@gmail.com>
2014-09-26 14:55:10 +0900

    달력컨트롤 추가, 등급아이콘 가이드 파일에 추가

--- js/event.js
+++ js/event.js
@@ -16,4 +16,92 @@ $(function(){
 		});
 	})();
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* 달력컨트롤 */
+	(function(){
+		var calendarArea = $('.search_date');
+		
+		calendarArea.find('input').datepicker({
+			duration: 'fast',
+			dateFormat: 'yy/mm/dd',
+			showMonthAfterYear: true,
+			monthNames: ['01','02','03','04','05','06','07','08','09','10','11','12'],
+			dayNamesMin: ['일','월','화','수','목','금','토']
+		});
+		calendarArea.find('.btn_start_cal').on('click',function(e) {
+			e.preventDefault();
+			calendarArea.find('.start_cal').datepicker('show');
+		});
+		calendarArea.find('.btn_end_cal').on('click',function(e) {
+			e.preventDefault();
+			calendarArea.find('.end_cal').datepicker('show');
+		});
+	})();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 });
